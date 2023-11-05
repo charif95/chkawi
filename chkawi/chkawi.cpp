806fc29d886f613feb7411419d45f6fd26f6eede
Charif <elkhamlichi95charif@gmail.com>
2023-11-05 17:49:59 +0100

    Cpp1

--- chkawi/chkawi.cpp
+++ chkawi/chkawi.cpp
@@ -7,14 +7,72 @@ using namespace std;
 
 int main()
 {
-	bool Result;
-
-	Result=!(12>=12)
+	bool Result, True; 
 
 
-	
+	cout << (12 >= 12) << endl;
+	cout << (12 > 7) << endl;
+	cout << (8 < 6) << endl;
+	cout << (8 == 8) << endl;
+	cout << (12 <= 12) << endl;
+	cout << (7 == 5) << endl << endl << endl;
 
-	
+
+	cout << !(12 >= 12) << endl;
+	cout << !(12 < 7) << endl;
+	cout << !(8 < 6) << endl;
+	cout << !(8 == 8) << endl;
+	cout << !(12 <= 12) << endl;
+	cout << !(7 == 5) << endl<< endl<< endl;
+
+
+
+	cout << (1 && 1) << endl;
+	cout << (true && 0) << endl;
+	cout << (0 || 1) << endl;
+	cout << (0 || 0) << endl;
+	cout << (!0) << endl;
+	cout << !(1 || 0) << endl << endl << endl;
+
+
+
+	Result = ((7 == 7) && (7 > 5));
+	cout << Result << endl;
+
+	Result= ((7 == 7) && (7 < 5));
+	cout << Result << endl; 
+
+	Result= ((7 == 7) || (7 < 5));
+	cout << Result << endl; 
+
+	Result= ((7 == 7) || (7 > 5));
+	cout << Result << endl; 
+
+	Result= (!(7 == 7) && (7 > 5));
+	cout << Result << endl; 
+
+	Result= ((7 == 7) && !(7 < 5));
+	cout << Result << endl << endl << endl;
+
+
+
+	Result = (5 > 6 && 7 == 7) || (1 || 0);
+	cout << Result << endl;
+
+	Result = !(5 > 6 && 7 == 7) || (1 || 0);
+	cout << Result << endl; 
+
+	Result = !(5 > 6 && 7 == 7) || !(1 || 0);
+	cout << Result << endl; 
+
+	Result = !(5 > 6 || 7 == 7) && !(1 || 0);
+	cout << Result << endl;
+
+	Result = ((5 > 6 && 7 <= 8) || (8 > 1 && 4 <= 3)) && true;
+	cout << Result << endl;
+
+	Result = ((5 > 6) && !(7 <= 8) && (8 > 1 || 4 <= 3)) || true;
+	cout << Result << endl; 
 
 }
 
